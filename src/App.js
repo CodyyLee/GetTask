989a16fb42efc087a00f736f652b6e0647fb7672
Cody Garrett <cody.l.garrett22@gmail.com>
2021-03-05 14:00:41 -0500

    tab screens swap correctly, still need to show tab switch

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 //data import
 import store from './store/store';
 
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -2,18 +2,39 @@ import React, { useState } from 'react';
 import { AppBar, Tabs, Tab } from '@material-ui/core';
 import store from '../../store/store';
 
+import Settings from '../tabs/Settings';
+import Tasks from '../tabs/Tasks';
+import Manage from '../tabs/Manage';
+
 export default function Navigation() {
 
+    const [curr, setCurr] = useState(0);
+
     const changeHandler = (e, i) => {
-        store.tab = i;
+        setCurr(i);
     }
 
     return (
-        <AppBar position='static'>
-            <Tabs value={store.tab} onChange={changeHandler}>
-                <Tab label='Get Task'/>
-                <Tab label='Settings'/>
-            </Tabs>
-        </AppBar>
+        <>
+            <AppBar position='static'>
+                <Tabs value={store.tab} onChange={changeHandler}>
+                    <Tab label='Get Task'/>
+                    <Tab label='Manage Tasks'/>
+                    <Tab label='Settings'/>
+                </Tabs>
+            </AppBar>
+
+            <div hidden={curr !== 0}>
+                <Tasks />
+            </div>
+
+            <div hidden={curr !== 1}>
+                <Manage />
+            </div>
+
+            <div hidden={curr !== 2}>
+                <Settings />
+            </div>
+        </>
     )
 }
